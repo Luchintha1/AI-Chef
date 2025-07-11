39fa49a7c33241ed00f0f8b7b1ec6035642d21db
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-11 16:21:48 +0530

    Add the form

---
+++ AI-Chef_Assistance/components/Main.jsx
@@ -0,0 +1,14 @@
+export function Main(){
+    return(
+        <main>
+            <form>
+                <input
+                type="text"
+                placeholder="e.g. Potato"
+                aria-lable="Add Ingredients"
+                />
+                <button>Add Ingredient</button>
+            </form>
+        </main>
+    );
+}
