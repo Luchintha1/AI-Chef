40fdb8044a3bb30d007a85010d55f1a110627413
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-11 16:04:55 +0530

    Header Completed

---
+++ AI-Chef_Assistance/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header(){
+    return(
+        <header>
+            <img src="../src/assets/logo.png" alt="Chefry AI Logo" />
+            <h1>Chefry AI</h1>
+        </header>
+    )
+}
