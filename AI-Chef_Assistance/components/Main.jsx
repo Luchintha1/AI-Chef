a37638fe2558514ee9725e5bd944544b6271cf2c
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-13 17:19:58 +0530

    Add states

--- AI-Chef_Assistance/components/Main.jsx
+++ AI-Chef_Assistance/components/Main.jsx
@@ -1,14 +1,35 @@
+import React from "react";
+
 export function Main(){
+
+    const [ingredients, set_ingredients] = React.useState([]);
+    
+    function handleSubmit(event){
+
+        event.preventDefault();
+
+        const form_data = new FormData(event.currentTarget);
+        const new_ingredient = form_data.get("ingredient");
+
+        set_ingredients((prev_ingredients) => [...prev_ingredients, new_ingredient]);
+    }
+
+    const ingredientListItem = ingredients.map((item) => <li key={item}>{item}</li>)
+
     return(
         <main>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                 type="text"
                 placeholder="e.g. Potato"
-                aria-lable="Add Ingredients"
+                aria-label="Add Ingredients"
+                name="ingredient"
                 />
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientListItem}
+            </ul>
         </main>
     );
 }
