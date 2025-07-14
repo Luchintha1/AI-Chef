8e63e8c493b2206c486a2f27d8ebab350a2f9b4b
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-14 14:34:19 +0530

    Add more sections to the form

--- AI-Chef_Assistance/components/Main.jsx
+++ AI-Chef_Assistance/components/Main.jsx
@@ -23,9 +23,15 @@ export function Main(){
                 />
                 <button>Add Ingredient</button>
             </form>
-            <ul>
-                {ingredientListItem}
-            </ul>
+            <h2>Ingredients on hand: </h2>
+            <ul className="ingredients-list" aria-live="polite">{ingredientListItem}</ul>
+            <div className="get-recipe-container">
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button>Generate a Recipe</button>
+            </div>
         </main>
     );
 }
